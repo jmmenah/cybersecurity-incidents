2348b045d027de73bfbf6a5265fb493194e68238
Juan Manuel Mena Hernández <55758232+jmmenah@users.noreply.github.com>
2021-12-04 00:05:32 +0100

    Add files via upload

---
+++ Phyton/tema-2/ex01.py
@@ -0,0 +1,33 @@
+#esto es un comentario
+
+print ("esto es una función que imprime por pantalla")
+print ("primer parámetro", "segundo parámetro")
+
+#precedencia de operador * / + -
+
+print ("pepe tiene ",2*6/3+5," años")
+
+#True y false son tipos booleanos
+pepe = 9
+juan = 6
+print ("es pepe mayor que juan?: ",pepe>juan)
+
+#tipos de variables
+
+#boolean
+pepe = True
+print ("Ahora pepe es booleano ", pepe)
+
+#entero
+pepe = 3 + 9
+print ("Ahora pepe es entero ", pepe)
+
+#float (decimal)
+pepe = 9 / 3
+print ("Ahora pepe es float ", pepe)
+#division siempre transforma a float
+#cualquier operacion entre entero y float da resultado float
+saluda = "hola"
+salu2 = "mundo"
+print (saluda+salu2)
+print('%s %s'%(saluda,salu2))
